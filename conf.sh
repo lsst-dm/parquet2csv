13ec0d242a96511ca63fea25bf5618dec66b46f3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-03-02 15:16:47 +0100

    Build from source

--- conf.sh
+++ conf.sh
@@ -1,6 +1,6 @@
 # Build parameters
 # ----------------
-readonly REPO="gitlab-registry.in2p3.fr/astrolabsoftware/fink"
+readonly REPO="gitlab-registry.in2p3.fr/qserv/parquet2csv/parquet2csv"
 # Tag to apply to the built image, or to identify the image to be pushed
 GIT_HASH="$(git -C $DIR describe --dirty --always)"
 readonly IMAGE_TAG="$GIT_HASH"
